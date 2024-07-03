055849bfef5bbd8c0966e76455611484965aa76b
Stephen Bletsas <stephenbletsas10.sb@gmail.com>
2024-07-03 22:12:08 +1000

    connected open ai to the system

--- ind_learning_system/app/page.js
+++ ind_learning_system/app/page.js
@@ -16,6 +16,8 @@ import Result from "./component/Result";
 import FeedbackBox from "./component/Feedback";
 import Header from "./component/Header";
 
+import { Message, useAssistant } from 'ai/react';
+
 const STARTING_TIME = 300000;
 
 const theme = createTheme({
@@ -102,6 +104,8 @@ export default function Home() {
 	const [questionFeedbackDurations, setQuestionFeedbackDurations] = useState([]);
 
 	const [mode, setMode] = useState('system1');
+
+	const { status, messages, input, submitMessage, handleInputChange } = useAssistant({ api: '/api/assistant' });
 
 	useEffect(() => {
 		get_questions().then(value => {
@@ -311,6 +315,34 @@ export default function Home() {
 						</div>
 					)
 				}
+				<div>
+					{messages.map((m) => (
+						<div key={m.id}>
+						<strong>{`${m.role}: `}</strong>
+						{m.role !== 'data' && m.content}
+						{m.role === 'data' && (
+							<>
+							{(m.data).description}
+							<br />
+							<pre className={'bg-gray-200'}>
+								{JSON.stringify(m.data, null, 2)}
+							</pre>
+							</>
+						)}
+						</div>
+					))}
+					
+					{status === 'in_progress' && <div />}
+
+					<form onSubmit={submitMessage}>
+						<input
+						disabled={status !== 'awaiting_message'}
+						value={input}
+						placeholder="What is the temperature in the living room?"
+						onChange={handleInputChange}
+						/>
+					</form>
+				</div>
 			</MainPaper>
 		</ThemeProvider>
 	);
